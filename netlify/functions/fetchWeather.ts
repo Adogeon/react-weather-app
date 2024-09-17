abc69df1416dce0a52f3a573d45fdc02b848f5ab
Anh Dao <addao823@gmail.com>
2024-09-16 20:16:06 -0700

    Add Netlify functions to handling fetch weather

--- netlify/functions/fetchWeather.ts
+++ netlify/functions/fetchWeather.ts
@@ -1,12 +1,35 @@
+import dotenv from 'dotenv';
 import fetch from 'node-fetch';
-import { Handler } from '@netlify/functions'
+import { Config, Context } from '@netlify/functions'
 
+dotenv.config();
+export default async function (reg: Request, context: Context) {
+    const { city } = context.params;
+    const API_KEY = process.env.WEATHER_API_KEY;
 
-const handler: Handler = async function (event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello World" })
+    if (!API_KEY) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Missing API key' }),
+        };
+    }
+
+    try {
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`);
+        const data = await response.json();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ msg: 'Failed to fetch weather data', error }),
+        };
     }
 }
 
-export { handler }
+export const config: Config = {
+    path: "/weather/:city"
+}
