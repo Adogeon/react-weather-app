63864ff5a078781ab7db36239e7a953be85c1898
Anh Dao <addao823@gmail.com>
2024-09-16 20:04:52 -0700

    First commit

---
+++ netlify/functions/fetchWeather.ts
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+import { Handler } from '@netlify/functions'
+
+
+const handler: Handler = async function (event, context) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello World" })
+    }
+}
+
+export { handler }
