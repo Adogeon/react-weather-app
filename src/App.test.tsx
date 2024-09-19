a4c94c86736850153a03ed50ac9d585c49ea6774
Anh Dao <addao823@gmail.com>
2024-09-19 09:38:57 -0700

    extend vitest matchers

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import { render, fireEvent, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("Load the App body on screen", async () => {
   render(<App />);
 
-  expect(screen.getByRole("link")).toBeInTheDocument();
+  expect(screen.getByRole("link")).toBeIntheDocument();
 });
